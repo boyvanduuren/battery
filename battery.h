9efee78a8c3038df1e435bfe7b027dd0c4c1fdf5
bcd <bcvanduuren@gmail.com>
2014-04-06 21:05:35 +0200

    Changed BAT_MAX to right path

--- battery.h
+++ battery.h
@@ -3,7 +3,7 @@
 #define POLL_AVG 20
 #define POLL_ITR (POLL_AVG / POLL_INT)
 #define BAT_CUR "/sys/class/power_supply/BAT0/charge_now"
-#define BAT_MAX "/sys/class/power_supply/BAT0/charge_full"
+#define BAT_MAX "/sys/class/power_supply/BAT0/charge_full_design"
 #define AC_CHRG "/sys/class/power_supply/AC/online"
 
 int getValue(const char *filename);
